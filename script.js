de655d6df0fafc366fa6ea57cb05be88e6641f2a
Khushi Kumari <keshri.khushi06@gmail.com>
2026-05-06 11:21:35 +0530

    Updated TO DO APP

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-function addTask() {
-  const input = document.getElementById("taskInput");
-  const taskText = input.value.trim();
+let addButton = document.querySelector(".fa-plus");
+let Tasks = [];
+let task_input = document.querySelector("#task_input");
 
-  if (taskText === "") return;
+let tasks_div = document.querySelector(".tasks");
+tasks_div.textContent = "Your Tasks will appear here";
 
-  const li = document.createElement("li");
+task_input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" || e.keyCode === 13) {
+    addButton.click();
+  }
+});
 
-  const span = document.createElement("span");
-  span.textContent = taskText;
+addButton.addEventListener("click", () => {
+  if (task_input.value.trim() === "") {
+    alert("Please enter your task to add");
+    return;
+  }
+  Tasks.push({ text: task_input.value.trim(), completed: false });
+  task_input.value = "";
+  renderTasks();
+});
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "X";
+function renderTasks() {
+  if (Tasks.length === 0) {
+    tasks_div.textContent = "Your Tasks will appear here";
+  } else {
+    tasks_div.innerHTML = "";
+    Tasks.forEach((task, index) => {
+      let task_div = document.createElement("div");
+      task_div.setAttribute("class", "task");
 
-  deleteBtn.onclick = function () {
-  li.remove();
-  };
+      let task_number = document.createElement("div");
+      let task_text = document.createElement("div");
+      task_text.setAttribute("class", "task_text");
 
-  li.appendChild(span);
-  li.appendChild(deleteBtn);
-  document.getElementById("taskList").appendChild(li);
-  input.value = "";
+      let checkbox = document.createElement("div");
+      checkbox.setAttribute("class", "checkbox");
+
+      let remove_button = document.createElement("div");
+      remove_button.setAttribute("class", "remove_button");
+
+      task_number.textContent = `Task No ${index + 1}:`;
+      task_text.textContent = task.text;
+      checkbox.innerHTML = task.completed
+        ? `<i class="fa-solid fa-circle-check"></i>`
+        : `<i class="fa-regular fa-circle-check"></i>`;
+      remove_button.innerHTML = `<i class="fa-solid fa-circle-minus"></i>`;
+
+      task_div.appendChild(task_number);
+      task_div.appendChild(task_text);
+      task_div.appendChild(checkbox);
+      task_div.appendChild(remove_button);
+
+      tasks_div.appendChild(task_div);
+
+      checkbox.addEventListener("click", () => {
+        task.completed = !task.completed;
+        checkbox.innerHTML = task.completed
+          ? `<i class="fa-solid fa-circle-check"></i>`
+          : `<i class="fa-regular fa-circle-check"></i>`;
+      });
+
+      remove_button.addEventListener("click", () => {
+        Tasks.splice(index, 1);
+        renderTasks();
+      });
+    });
+  }
 }
